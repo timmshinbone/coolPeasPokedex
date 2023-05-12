f7532f0c3842a2ea3e1a40b5519d7629ba690293
bethparnell <beth.parnell@generalassemb.ly>
2023-05-12 15:38:02 -0400

    Finished code along

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,4 +1,5 @@
-
+// All of the action we are going to be doing are going to be on the BattleTeams model
+const BattleTeam = require('../models/battle-team')
 
 function show(req, res, next) {
     // we are going to be searching for the pokemon on their name not id
@@ -13,6 +14,46 @@ function show(req, res, next) {
         .catch(next)
 }
 
+// Add a pokemon to a battle team
+// CREATE - CRUD
+function addPokemonToTeam(req, res, next) {
+    // query for a team to add the pokemon to
+    // push my pokemon document into the pokemon field
+    // save my document
+    // redirect to the battle team that I found and updated
+
+    BattleTeam.findById(req.params.battleId)
+        .then(battleTeam => {
+            // { name: 'user value' }
+            battleTeam.pokemon.push(req.body)
+
+            return battleTeam.save()
+        })
+        .then(() => res.redirect(`/battle-teams/${req.params.battleId}`))
+        .catch(next)
+}
+
+// DELETE - CRUD
+function deletePokemonFromTeam(req, res, next) {
+    // find a battle team
+    // check is the current user is that teams owner
+    // remove the pokemon from the team
+    // save the battle team document after deleting
+    // redirect back to the battle team show page
+
+    BattleTeam.findById(req.params.battleId)
+        .then(battleTeam => {
+            if (!battleTeam.user.equals(req.user._id)) throw new Error('Unauthorized')
+
+            battleTeam.pokemon.id(req.params.pokemonId).deleteOne()
+            return battleTeam.save()
+        })
+        .then(() => res.redirect(`/battle-teams/${req.params.battleId}`))
+        .catch(next)
+}
+
 module.exports = {
-    show
+    show,
+    addPokemonToTeam,
+    deletePokemonFromTeam
 }
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -4,5 +4,10 @@ const router = express.Router()
 const pokemonCtrl = require('../controllers/pokemon')
 
 router.get('/:name', pokemonCtrl.show)
+router.post('/:battleId', pokemonCtrl.addPokemonToTeam)
+// we need 2 bits of info here
+// we need the Battle Teams ID
+// We also need the pokemon ID
+router.delete('/:battleId/:pokemonId', pokemonCtrl.deletePokemonFromTeam)
 
 module.exports = router
